3a8f465be8dda64c051bc7ce10522649717a81ed
kavtsevich valeria <kavtsevichlera@gmail.com>
2024-12-16 21:02:19 +0300

    englishput

---
+++ Lera/проектопи/Source.cpp
@@ -0,0 +1,279 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void englishput()
+
+{
+		ofstream word7;
+		word7.open("english7.txt");
+		if (word7.is_open())
+		{
+			word7 << "Academy" << endl
+				<< "Against" << endl
+				<< "Arrange" << endl
+				<< "Balloon" << endl
+				<< "Balance" << endl
+				<< "Beneath" << endl
+				<< "Blanket" << endl
+				<< "Brother" << endl
+				<< "Capture" << endl
+				<< "Channel" << endl
+				<< "Charter" << endl
+				<< "College" << endl
+				<< "Comrade" << endl
+				<< "Concern" << endl
+				<< "Connect" << endl
+				<< "Content" << endl
+				<< "Courage" << endl
+				<< "Crystal" << endl
+				<< "Current" << endl
+				<< "Digital" << endl
+				<< "Disease" << endl
+				<< "Display" << endl
+				<< "Drawing" << endl
+				<< "Evening" << endl
+				<< "Example" << endl
+				<< "Fashion" << endl
+				<< "Feature" << endl
+				<< "Fitness" << endl
+				<< "General" << endl
+				<< "History" << endl;
+
+		}
+		word7.close();
+
+		ofstream word6;
+		word6.open("english6.txt");
+		if (word6.is_open())
+		{
+			word6 << "Animal" << endl
+				<< "Bottle" << endl
+				<< "Bridge" << endl
+				<< "Camera" << endl
+				<< "Castle" << endl
+				<< "Circle" << endl
+				<< "Copper" << endl
+				<< "Couple" << endl
+				<< "Course" << endl
+				<< "Custom" << endl
+				<< "Danger" << endl
+				<< "Double" << endl
+				<< "Energy" << endl
+				<< "Family" << endl
+				<< "Farmer" << endl
+				<< "Faster" << endl
+				<< "Forest" << endl
+				<< "Friend" << endl
+				<< "Future" << endl
+				<< "Garden" << endl
+				<< "Golden" << endl
+				<< "Ground" << endl
+				<< "Growth" << endl
+				<< "Hidden" << endl
+				<< "Hunter" << endl
+				<< "Inside" << endl
+				<< "Island" << endl
+				<< "Jungle" << endl
+				<< "Kindly" << endl
+				<< "Kitten" << endl;
+
+		}
+		word6.close();
+
+		ofstream word5;
+		word5.open("english5.txt");
+		if (word5.is_open())
+		{
+			word5 << "Apple" << endl
+				<< "Beach" << endl
+				<< "Bread" << endl
+				<< "Brain" << endl
+				<< "Crown" << endl
+				<< "Dance" << endl
+				<< "Dream" << endl
+				<< "Earth" << endl
+				<< "Field" << endl
+				<< "Flame" << endl
+				<< "Fruit" << endl
+				<< "Glass" << endl
+				<< "Grant" << endl
+				<< "Green" << endl
+				<< "Heart" << endl
+				<< "Horse" << endl
+				<< "House" << endl
+				<< "Layer" << endl
+				<< "Light" << endl
+				<< "Magic" << endl
+				<< "Metal" << endl
+				<< "Night" << endl
+				<< "Ocean" << endl
+				<< "Paint" << endl
+				<< "Paper" << endl
+				<< "Plant" << endl
+				<< "Power" << endl
+				<< "River" << endl
+				<< "Score" << endl
+				<< "Shine" << endl;
+
+		}
+		word5.close();
+
+		ofstream word4;
+		word4.open("english4.txt");
+		if (word4.is_open())
+		{
+			word4 << "Arch" << endl
+				<< "Army" << endl
+				<< "Bark" << endl
+				<< "Bell" << endl
+				<< "Bird" << endl
+				<< "Book" << endl
+				<< "Boot" << endl
+				<< "Cake" << endl
+				<< "Card" << endl
+				<< "Cart" << endl
+				<< "City" << endl
+				<< "Claw" << endl
+				<< "Cliff" << endl
+				<< "Club" << endl
+				<< "Coat" << endl
+				<< "Corn" << endl
+				<< "Cube" << endl
+				<< "Dawn" << endl
+				<< "Deer" << endl
+				<< "Dome" << endl
+				<< "Door" << endl
+				<< "Dust" << endl
+				<< "Face" << endl
+				<< "Fair" << endl
+				<< "Farm" << endl
+				<< "Fire" << endl
+				<< "Fish" << endl
+				<< "Flag" << endl
+				<< "Flow" << endl
+				<< "Food" << endl;
+		}
+		word4.close();
+
+		ofstream word3;
+		word3.open("english3.txt");
+		if (word3.is_open())
+		{
+			word3 << "Air" << endl
+				<< "Ant" << endl
+				<< "Bat" << endl
+				<< "Bee" << endl
+				<< "Bit" << endl
+				<< "Box" << endl
+				<< "Boy" << endl
+				<< "Bus" << endl
+				<< "But" << endl
+				<< "Cab" << endl
+				<< "Car" << endl
+				<< "Cat" << endl
+				<< "Cow" << endl
+				<< "Cut" << endl
+				<< "Day" << endl
+				<< "Dog" << endl
+				<< "Dot" << endl
+				<< "Dry" << endl
+				<< "Ear" << endl
+				<< "Egg" << endl
+				<< "End" << endl
+				<< "Far" << endl
+				<< "Fat" << endl
+				<< "Fax" << endl
+				<< "Fix" << endl
+				<< "Fly" << endl
+				<< "For" << endl
+				<< "Fun" << endl
+				<< "Gas" << endl
+				<< "Get" << endl;
+		}
+		word3.close();
+
+		ofstream word2;
+		word2.open("english2.txt");
+		if (word2.is_open())
+		{
+			word2 << "Am" << endl
+				<< "An" << endl
+				<< "As" << endl
+				<< "At" << endl
+				<< "Ax" << endl
+				<< "Ay" << endl
+				<< "Ba" << endl
+				<< "Be" << endl
+				<< "Bi" << endl
+				<< "By" << endl
+				<< "Do" << endl
+				<< "Ed" << endl
+				<< "Ef" << endl
+				<< "Eh" << endl
+				<< "El" << endl
+				<< "Em" << endl
+				<< "En" << endl
+				<< "Er" << endl
+				<< "Es" << endl
+				<< "Et" << endl
+				<< "Ex" << endl
+				<< "Go" << endl
+				<< "Ha" << endl
+				<< "He" << endl
+				<< "Hi" << endl
+				<< "Ho" << endl
+				<< "Id" << endl
+				<< "If" << endl
+				<< "In" << endl
+				<< "Is" << endl;
+		}
+		word2.close();
+		ofstream word1;
+		word1.open("english1.txt");
+		if (word1.is_open())
+		{
+			word1 << "A" << endl
+				<< "B" << endl
+				<< "C" << endl
+				<< "D" << endl
+				<< "E" << endl
+				<< "F" << endl
+				<< "G" << endl
+				<< "H" << endl
+				<< "I" << endl
+				<< "J" << endl
+				<< "K" << endl
+				<< "L" << endl
+				<< "M" << endl
+				<< "N" << endl
+				<< "O" << endl
+				<< "P" << endl
+				<< "Q" << endl
+				<< "R" << endl
+				<< "S" << endl
+				<< "T" << endl
+				<< "U" << endl
+				<< "V" << endl
+				<< "W" << endl
+				<< "X" << endl
+				<< "Y" << endl
+				<< "Z" << endl
+				<< "a" << endl
+				<< "b" << endl
+				<< "c" << endl
+				<< "d" << endl;
+		}
+		word1.close();
+}
+
+int main()
+{
+	 englishput();
+}
+	
+
+
+
+
---
+++ Lera/проектопи/опизаписьанглсловвфайл.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+
+{
+	ofstream word7;
+	word7.open("symbol7.txt");
+	if (word7.is_open())
+	{
+		word7 << "Academy" << ' ' << "Against" << ' ' << "Arrange" << ' ' << "Balloon" << ' ' << "Balance" << ' ' << "Beneath" << ' ' << "Blanket" << ' ' << "Brother" << ' ' << "Capture" << ' ' << "Channel" << ' ' << "Charter" << ' ' << "College" << ' ' << "Comrade" << ' ' << "Concern" << ' ' << "Connect" << ' ' << "Content" << ' ' << "Courage" << ' ' << "Crystal" << ' ' << "Current" << ' ' << "Digital" << ' ' << "Disease" << ' ' << "Display" << ' ' << "Drawing" << ' ' << "Evening" << ' ' << "Example" << ' ' << "Fashion" << ' ' << "Feature" << ' ' << "Fitness" << ' ' << "General" << ' ' << "History" << ' ' << "Imagine" << ' ' << "Insider" << ' ' << "Journal" << ' ' << "Justice" << ' ' << "Lantern" << ' ' << "Leading" << ' ' << "Leather" << ' ' << "Library" << ' ' << "Limited" << ' ' << "Literacy" << ' ' << "Machine" << ' ' << "Manager" << ' ' << "Measure" << ' ' << "Medical" << ' ' << "Message" << ' ' << "Mineral" << ' ' << "Mission" << ' ' << "Mystery" << ' ' << "Natural" << ' ' << "Network" << ' ' << "Nothing" << ' ' << "Offshore" << ' ' << "Opinion" << ' ' << "Organic" << ' ' << "Outside" << ' ' << "Painter" << ' ' << "Pattern" << ' ' << "Perfect" << ' ' << "Picture" << ' ' << "Pleasure" << ' ' << "Popular" << ' ' << "Premium" << ' ' << "Present" << ' ' << "Process" << ' ' << "Program" << ' ' << "Purpose" << ' ' << "Quality" << ' ' << "Reality" << ' ' << "Refugee" << ' ' << "Regular" << ' ' << "Related" << ' ' << "Respect" << ' ' << "Revenue" << ' ' << "Romance" << ' ' << "Scholar" << ' ' << "Science" << ' ' << "Section" << ' ' << "Segment" << ' ' << "Service" << ' ' << "Similar" << ' ' << "Speaker" << ' ' << "Special" << ' ' << "Stadium" << ' ' << "Station" << ' ' << "Storage" << ' ' << "Student" << ' ' << "Success" << ' ' << "Summary" << ' ' << "Support" << ' ' << "Survive" << ' ' << "Teacher" << ' ' << "Theater" << ' ' << "Trouble" << ' ' << "Uniform" << ' ' << "Unusual" << ' ' << "Village" << ' ' << "Waiting" << ' ' << "Warrior" << ' ' << "Weather" << ' ' << "Written";
+	}
+	word7.close();
+
+	ofstream word6;
+	word6.open("symbol6.txt");
+	if (word6.is_open())
+	{
+		word6 << "Animal" << ' ' << "Bottle" << ' ' << "Bridge" << ' ' << "Camera" << ' ' << "Castle" << ' ' << "Circle" << ' ' << "Copper" << ' ' << "Couple" << ' ' << "Course" << ' ' << "Custom" << ' ' << "Danger" << ' ' << "Double" << ' ' << "Energy" << ' ' << "Family" << ' ' << "Farmer" << ' ' << "Faster" << ' ' << "Forest" << ' ' << "Friend" << ' ' << "Future" << ' ' << "Garden" << ' ' << "Golden" << ' ' << "Ground" << ' ' << "Growth" << ' ' << "Hidden" << ' ' << "Hunter" << ' ' << "Inside" << ' ' << "Island" << ' ' << "Jungle" << ' ' << "Kindly" << ' ' << "Kitten" << ' ' << "Ladder" << ' ' << "Leader" << ' ' << "Little" << ' ' << "London" << ' ' << "Market" << ' ' << "Master" << ' ' << "Matter" << ' ' << "Member" << ' ' << "Memory" << ' ' << "Middle" << ' ' << "Modern" << ' ' << "Mother" << ' ' << "Nature" << ' ' << "Office" << ' ' << "Online" << ' ' << "Orange" << ' ' << "Parent" << ' ' << "People" << ' ' << "Pocket" << ' ' << "Pretty" << ' ' << "Public" << ' ' << "Reader" << ' ' << "Record" << ' ' << "Remote" << ' ' << "Return" << ' ' << "Rocket" << ' ' << "Silver" << ' ' << "Simple" << ' ' << "Singer" << ' ' << "Sister" << ' ' << "Smooth" << ' ' << "Spider" << ' ' << "Square" << ' ' << "Stable" << ' ' << "Street" << ' ' << "Strong" << ' ' << "Summer" << ' ' << "Supply" << ' ' << "Talent" << ' ' << "Target" << ' ' << "Travel" << ' ' << "Unique" << ' ' << "United" << ' ' << "Valley" << ' ' << "Vision" << ' ' << "Walker" << ' ' << "Weapon" << ' ' << "Wealth" << ' ' << "Winner" << ' ' << "Winter" << ' ' << "Wonder" << ' ' << "Worker" << ' ' << "Writer" << ' ' << "Yellow";
+	}
+	word6.close();
+
+	ofstream word5;
+	word5.open("symbol5.txt");
+	if (word5.is_open())
+	{
+		word5 << "Apple" << ' ' << "Beach" << ' ' << "Bread" << ' ' << "Brain" << ' ' << "Crown" << ' ' << "Dance" << ' ' << "Dream" << ' ' << "Earth" << ' ' << "Field" << ' ' << "Flame" << ' ' << "Fruit" << ' ' << "Glass" << ' ' << "Grant" << ' ' << "Green" << ' ' << "Heart" << ' ' << "Horse" << ' ' << "House" << ' ' << "Layer" << ' ' << "Light" << ' ' << "Magic" << ' ' << "Metal" << ' ' << "Night" << ' ' << "Ocean" << ' ' << "Paint" << ' ' << "Paper" << ' ' << "Plant" << ' ' << "Power" << ' ' << "River" << ' ' << "Score" << ' ' << "Shine" << ' ' << "Shore" << ' ' << "Smart" << ' ' << "Space" << ' ' << "Spark" << ' ' << "Stone" << ' ' << "Sugar" << ' ' << "Table" << ' ' << "Theme" << ' ' << "Tiger" << ' ' << "Train" << ' ' << "Trust" << ' ' << "Voice" << ' ' << "Water" << ' ' << "Wheat" << ' ' << "World" << ' ' << "Wound" << ' ' << "Youth" << ' ' << "Zebra" << ' ' << "Agent" << ' ' << "Angle" << ' ' << "Arrow" << ' ' << "Basic" << ' ' << "Beast" << ' ' << "Bless" << ' ' << "Bliss" << ' ' << "Block" << ' ' << "Brick" << ' ' << "Build" << ' ' << "Chair" << ' ' << "Chain" << ' ' << "Chest" << ' ' << "Chill" << ' ' << "Claim" << ' ' << "Clear" << ' ' << "Clock" << ' ' << "Close" << ' ' << "Cloud" << ' ' << "Coast" << ' ' << "Cover" << ' ' << "Craft" << ' ' << "Cream" << ' ' << "Crisp" << ' ' << "Crowd" << ' ' << "Crown" << ' ' << "Daily" << ' ' << "Drive" << ' ' << "Dried" << ' ' << "Drink" << ' ' << "Eagle" << ' ' << "Early" << ' ' << "Empty" << ' ' << "Enemy" << ' ' << "Entry" << ' ' << "Error" << ' ' << "Event" << ' ' << "Faith" << ' ' << "Fancy" << ' ' << "Fault" << ' ' << "Fiber" << ' ' << "Final" << ' ' << "Flute" << ' ' << "Focus" << ' ' << "Force" << ' ' << "Frame" << ' ' << "Front" << ' ' << "Fruit";
+	}
+	word5.close();
+
+	ofstream word4;
+	word4.open("symbol4.txt");
+	if (word4.is_open())
+	{
+		word4 << "Arch" << ' ' << "Army" << ' ' << "Bark" << ' ' << "Bell" << ' ' << "Bird" << ' ' << "Book" << ' ' << "Boot" << ' ' << "Cake" << ' ' << "Card" << ' ' << "Cart" << ' ' << "City" << ' ' << "Claw" << ' ' << "Cliff" << ' ' << "Club" << ' ' << "Coat" << ' ' << "Corn" << ' ' << "Cube" << ' ' << "Dawn" << ' ' << "Deer" << ' ' << "Dome" << ' ' << "Door" << ' ' << "Dust" << ' ' << "Face" << ' ' << "Fair" << ' ' << "Farm" << ' ' << "Fire" << ' ' << "Fish" << ' ' << "Flag" << ' ' << "Flow" << ' ' << "Food" << ' ' << "Fork" << ' ' << "Frog" << ' ' << "Gate" << ' ' << "Glow" << ' ' << "Gold" << ' ' << "Grin" << ' ' << "Hand" << ' ' << "Hawk" << ' ' << "Head" << ' ' << "Hill" << ' ' << "Home" << ' ' << "Hope" << ' ' << "Horn" << ' ' << "Idea" << ' ' << "Iron" << ' ' << "Jail" << ' ' << "Joke" << ' ' << "King" << ' ' << "Kite" << ' ' << "Lake" << ' ' << "Lamp" << ' ' << "Land" << ' ' << "Leaf" << ' ' << "Life" << ' ' << "Line" << ' ' << "Lion" << ' ' << "Luck" << ' ' << "Mask" << ' ' << "Moon" << ' ' << "Nest" << ' ' << "Path" << ' ' << "Plan" << ' ' << "Play" << ' ' << "Rain" << ' ' << "Ring" << ' ' << "Rock" << ' ' << "Roof" << ' ' << "Rose" << ' ' << "Rule" << ' ' << "Sand" << ' ' << "Seed" << ' ' << "Ship" << ' ' << "Shore" << ' ' << "Side" << ' ' << "Snow" << ' ' << "Song" << ' ' << "Star" << ' ' << "Stay" <<  ' ' << "Surf" << ' ' << "Swim" << ' ' << "Task" << ' ' << "Team" << ' ' << "Tent" << ' ' << "Time" << ' ' << "Tree" << ' ' << "Wave" << ' ' << "Wind" << ' ' << "Wing" << ' ' << "Wolf" << ' ' << "Wood";
+	}
+	word4.close();
+
+	ofstream word3;
+	word3.open("symbol3.txt");
+	if (word3.is_open())
+	{
+		word3 << "Air" << ' ' << "Ant" << ' ' << "Bat" << ' ' << "Bee" << ' ' << "Bit" << ' ' << "Box" << ' ' << "Boy" << ' ' << "Bus" << ' ' << "But" << ' ' << "Cab" << ' ' << "Car" << ' ' << "Cat" << ' ' << "Cow" << ' ' << "Cut" << ' ' << "Day" << ' ' << "Dog" << ' ' << "Dot" << ' ' << "Dry" << ' ' << "Ear" << ' ' << "Egg" << ' ' << "End" << ' ' << "Far" << ' ' << "Fat" << ' ' << "Fax" << ' ' << "Fix" << ' ' << "Fly" << ' ' << "For" << ' ' << "Fun" << ' ' << "Gas" << ' ' << "Get" << ' ' << "God" << ' ' << "Got" << ' ' << "Gun" << ' ' << "Had" << ' ' << "Hat" << ' ' << "Her" << ' ' << "Hit" << ' ' << "Hot" << ' ' << "How" << ' ' << "Hut" << ' ' << "Ink" << ' ' << "Jam" << ' ' << "Jet" << ' ' << "Job" << ' ' << "Joy" << ' ' << "Key" << ' ' << "Lab" << ' ' << "Lap" << ' ' << "Led" << ' ' << "Leg" << ' ' << "Let" << ' ' << "Lip" << ' ' << "Log" << ' ' << "Lot" << ' ' << "Man" << ' ' << "Map" << ' ' << "Mat" << ' ' << "Men" << ' ' << "Mud" << ' ' << "Net" << ' ' << "Now" << ' ' << "Nut" << ' ' << "One" << ' ' << "Opt" << ' ' << "Out" << ' ' << "Own" << ' ' << "Owl" << ' ' << "Pan" << ' ' << "Pat" << ' ' << "Pen" << ' ' << "Pet" << ' ' << "Pig" << ' ' << "Pin" << ' ' << "Pot" << ' ' << "Pup" << ' ' << "Rat" << ' ' << "Red" << ' ' << "Rep" << ' ' << "Rig" << ' ' << "Run" << ' ' << "Sad" << ' ' << "Sat" << ' ' << "See" << ' ' << "Set" << ' ' << "Sit" << ' ' << "Sky" << ' ' << "Sly" << ' ' << "Sun" << ' ' << "Tap" << ' ' << "Tat" << ' ' << "Tea" << ' ' << "Tie" << ' ' << "Tip" << ' ' << "Too" << ' ' << "Top" << ' ' << "Toy" << ' ' << "Try" << ' ' << "Use" << ' ' << "Van" << ' ' << "Vat" << ' ' << "Vet" << ' ' << "Was" << ' ' << "Wax" << ' ' << "Web" << ' ' << "Wed" << ' ' << "Who" << ' ' << "Why" << ' ' << "Win" << ' ' << "Won" << ' ' << "Yes" << ' ' << "You";
+	}
+	word3.close();
+
+	ofstream word2;
+	word2.open("symbol2.txt");
+	if (word2.is_open())
+	{
+		word2 << "Am" << ' ' << "An" << ' ' << "As" << ' ' << "At" << ' ' << "Ax" << ' ' << "Ay" << ' ' << "Ba" << ' ' << "Be" << ' ' << "Bi" << ' ' << "By" << ' ' << "Do" << ' ' << "Ed" << ' ' << "Ef" << ' ' << "Eh" << ' ' << "El" << ' ' << "Em" << ' ' << "En" << ' ' << "Er" << ' ' << "Es" << ' ' << "Et" << ' ' << "Ex" << ' ' << "Go" << ' ' << "Ha" << ' ' << "He" << ' ' << "Hi" << ' ' << "Ho" << ' ' << "Id" << ' ' << "If" << ' ' << "In" << ' ' << "Is" << ' ' << "It" << ' ' << "Jo" << ' ' << "Ka" << ' ' << "La" << ' ' << "Li" << ' ' << "Lo" << ' ' << "Ma" << ' ' << "Me" << ' ' << "Mu" << ' ' << "My" << ' ' << "No" << ' ' << "Nu" << ' ' << "Od" << ' ' << "OE" << ' ' << "Of" << ' ' << "Oh" << ' ' << "Oi" << ' ' << "Om" << ' ' << "On" << ' ' << "Op" << ' ' << "Or" << ' ' << "Os" << ' ' << "Ox" << ' ' << "Oy" << ' ' << "Pa" << ' ' << "Pe" << ' ' << "Pi" << ' ' << "Re" << ' ' << "Sh" << ' ' << "Si" << ' ' << "So" << ' ' << "Ta" << ' ' << "To" << ' ' << "Uh" << ' ' << "Um" << ' ' << "Un" << ' ' << "Up" << ' ' << "Us" << ' ' << "We" << ' ' << "Wo" << ' ' << "Xi" << ' ' << "Ye" << ' ' << "Yo" << ' ' << "Za";
+	}
+	word2.close();
+
+	ofstream word1;
+	word1.open("symbol1.txt");
+	if (word1.is_open())
+	{
+		word1 << "A" << ' ' << "B" << ' ' << "C" << ' ' << "D" << ' ' << "E" << ' ' << "F" << ' ' << "G" << ' ' << "H" << ' ' << "I" << ' ' << "J" << ' ' << "K" << ' ' << "L" << ' ' << "M" << ' ' << "N" << ' ' << "O" << ' ' << "P" << ' ' << "Q" << ' ' << "R" << ' ' << "S" << ' ' << "T" << ' ' << "U" << ' ' << "V" << ' ' << "W" << ' ' << "X" << ' ' << "Y" << ' ' << "Z" << ' ' << "a" << ' ' << "b" << ' ' << "c" << ' ' << "d" << ' ' << "e" << ' ' << "f" << ' ' << "g" << ' ' << "h" << ' ' << "i" << ' ' << "j" << ' ' << "k" << ' ' << "l" << ' ' << "m" << ' ' << "n" << ' ' << "o" << ' ' << "p" << ' ' << "q" << ' ' << "r" << ' ' << "s" << ' ' << "t" << ' ' << "u" << ' ' << "v" << ' ' << "w" << ' ' << "x" << ' ' << "y" << ' ' << "z";
+	}
+	word1.close();
+}
+
